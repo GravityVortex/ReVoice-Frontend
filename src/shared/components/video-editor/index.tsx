a421fb90d8a51b2251095764c680a6e531d983c2
xuww <541765907@qq.com>
2025-12-22 10:34:19 +0800

    重构并编译

--- src/shared/components/video-editor/index.tsx
+++ src/shared/components/video-editor/index.tsx
@@ -1,3 +1,2 @@
-// Re-export the main VideoEditor component
-export { VideoEditor as default } from '../../../app/[locale]/(landing)/video_convert/video-editor/[id]/panel-video-editor';
+export { PanelVideoEditor as default } from '../../../app/[locale]/(landing)/video_convert/video-editor/[id]/panel-video-editor';
 export * from './types';
